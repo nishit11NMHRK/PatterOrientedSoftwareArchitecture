c87349c24fa79b164d637997e77b65b01ee6c8f1
PIRATE <nishit.singh@dlh.de>
2020-04-23 14:04:30 +0200

    Adapter Pattern

---
+++ adapter_EX_2/IndianThreePin.java
@@ -0,0 +1,13 @@
+package adapter_EX_2;
+
+public class IndianThreePin implements ThreePin
+{
+	
+	@Override
+	public void threepin() 
+	{
+		System.out.println("Indian Three Pin Plug");
+		
+	}
+
+}
